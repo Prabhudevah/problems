25ae241dc016caed470e694a929eba629fefa03a
Prabhu <Prabhudevah003@gmail.com>
2025-07-10 12:32:36 +0530

    third commit

--- javascript_object_assignment.js/day4/day7/loops.js
+++ javascript_object_assignment.js/day4/day7/loops.js
@@ -6,9 +6,10 @@
 // console.log(num);
 
 
-const num1=[2, 3, 4, 56, 78, 9, 7];
-const num2= num1.map(myFunction);
-function myFunction(value, index, array) {
-    return value * 5;
-}
-console.log(num2);
+// const num1=[2, 3, 4, 56, 78, 9, 7];
+// const num2= num1.map(myFunction);
+// function myFunction(value, index, array) {
+//     return value * 5;
+// }
+// console.log(num2);
+

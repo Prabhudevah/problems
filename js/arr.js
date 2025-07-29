43702eaea44c51c4d070d5822523f9ceec4bb281
Prabhu <Prabhudevah003@gmail.com>
2025-07-29 21:33:27 +0530

    self

---
+++ js/arr.js
@@ -0,0 +1,14 @@
+// 1st aray
+let selectcolor=['blue','red','ornsge']
+selectcolor[2]=1;
+console.log(selectcolor.length);
+
+
+//2nd pog function
+function greet(name,lastname){
+    console.log('hey gm' + name + ' '+ lastname); 
+    
+}
+
+greet('prabhu','hiremath');
+greet('pppp','hohiug');
---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+// let name='prabhuu';
+// let age=30;
+// let isApproved=false;
+// let firstName=undefined;
+// let selectedcolor=null;
+// console.log(name,age,isApproved);
+
+// let name='prabhu';
+// let age=22;
+
+//2nd objects 
+ let men={
+    name:'prabhu',
+    age:23
+ };
+
+
+ //dot notation
+
+ men.name='mom';
+console.log(men.name);
+
+
+
+
